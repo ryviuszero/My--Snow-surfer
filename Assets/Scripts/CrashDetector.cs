0eefbf8eed0e7d31b17abccb09b7fe005c6675c0
mywind <ryviuszero@gmail.com>
2025-08-25 15:32:13 +0800

    Using Invoke() For Delays

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,13 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 2f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
-       
-       if (other.gameObject.layer == layerIndex)
+
+        if (other.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            // SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,14 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 2f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
-       
-       if (other.gameObject.layer == layerIndex)
+
+        if (other.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            // SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 
 }
