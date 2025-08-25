497a6dea0af98c03e16ed932ecfec30a9c92b6a1
mywind <ryviuszero@gmail.com>
2025-08-25 17:08:01 +0800

    Using FindFirstObjectByType()

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,33 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float boostSpeed = 20f;
+
+    Vector2 moveVector;
+
+
     InputAction moveAction;
     Rigidbody2D myRigidbody2D;
+    SurfaceEffector2D surfaceEffector2D;
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     void Update()
     {
-        Vector2 moveVector;
+
+
+    }
+
+    void RotationPlayer()
+    {
+        
         moveVector = moveAction.ReadValue<Vector2>();
-        // print(moveVector);
-        // Debug.Log(moveVector);
         if (moveVector.x < 0)
         {
             myRigidbody2D.AddTorque(torqueAmount);
@@ -27,6 +39,18 @@ public class PlayerController : MonoBehaviour
         {
             myRigidbody2D.AddTorque(-torqueAmount);
         }
+    }
 
+    void BoostPlayer()
+    {
+
+        if ( moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
     }
 }
